5d0c89730ddc720b58a7d0916463d8fba0dc9f88
terminator637 <terminator637@gmail.com>
2018-04-25 00:37:25 +0300

    Использовал EasyJson, осталось сократить использование памяти в 2 раза.

--- fast.go
+++ fast.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
 	"strings"
+	"hw3_bench/myPackage"
 )
 
-type User struct {
-	Name     string   `json:"name"`
-	Email    string   `json:"email"`
-	Browsers []string `json:"browsers"`
-}
-
-// вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	file, err := os.Open(filePath)
+	fileContents, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		panic(err)
 	}
-
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
-
-	user := User{}
-	var line []byte
-	var i int
+	user := &myPackage.User{}
 	foundUsers := ""
-	lines := bytes.Split(fileContents, []byte("\n"))
-	seenBrowsers := make([]string,0)
-	for i, line = range lines {
-		if !bytes.Contains(line, []byte("Android")) && !bytes.Contains(line, []byte("MSIE")){
+	seenBrowsers := make(map[string]bool)
+	for i, line := range bytes.Split(fileContents, []byte("\n")) {
+		if !bytes.Contains(line, []byte("Android")) && !bytes.Contains(line, []byte("MSIE")) {
 			continue
 		}
 		// fmt.Printf("%v %v\n", err, line)
-		err := json.Unmarshal(line, &user)
+		err = user.UnmarshalJSON(line)
 		if err != nil {
 			panic(err)
 		}
@@ -57,19 +40,7 @@ func FastSearch(out io.Writer) {
 			default:
 				continue
 			}
-
-			notSeenBefore := true
-			for _, item := range seenBrowsers {
-				if item == browser {
-					notSeenBefore = false
-					break
-				}
-			}
-			if notSeenBefore {
-				// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
-				seenBrowsers = append(seenBrowsers, browser)
-			}
-
+			seenBrowsers[browser] = true
 		}
 
 		if !(isAndroid && isMSIE) {
---
+++ myPackage/package.go
@@ -0,0 +1,8 @@
+package myPackage
+
+
+type User struct {
+	Name     string   `json:"name"`
+	Email    string   `json:"email"`
+	Browsers []string `json:"browsers"`
+}
